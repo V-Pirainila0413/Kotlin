8ed955b83a1c3000eec0150491b26ca703e070cd
V.Pirainila <vinayakamoorthypirainila@gmail.com>
2026-04-30 19:01:32 +0530

    Add files via upload

---
+++ variable.kt.kt
@@ -0,0 +1,12 @@
+fun main() {
+
+    val name = "Ravi"   // immutable variable
+    var age = 20        // mutable variable
+
+    println(name)
+    println(age)
+
+    age = 25   // value changed
+
+    println(age)
+}
